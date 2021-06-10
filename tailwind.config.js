4687b9e5d7e4184dd50fe5aac5d35d67cc3d4f35
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-06-10 22:36:38 +0800

    configure font-faces

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+        header: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
