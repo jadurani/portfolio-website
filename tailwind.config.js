55c95d6cea71dca77a5b1f4ae7bd66da52cf39a2
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-06-10 22:41:19 +0800

    chore: config (#1)
    # Changes
    
    - installed prettier (2.2.1), husky(4.3.8), tslintconfig(1.18.0), tailwind(2.1.4)
    - upgraded angular and related packages
    - added pre-commit command (via husky)
    - set up font-faces to `Montserrat` and `Ubuntu`
    
    ## Notes
    
    - On updating angular, I used the following command:
    
    ```bash
      ng update @angular/cli --migrate-only --from 11 --to 12
    ```
    
    [Source](https://stackoverflow.com/a/67598492)

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+        header: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
