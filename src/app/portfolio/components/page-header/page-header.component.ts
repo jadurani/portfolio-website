ebfc5edb2c1050d5eceb249369e8f187be3939cb
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-04-06 13:29:10 +0800

    page headers

--- src/app/portfolio/components/page-header/page-header.component.ts
+++ src/app/portfolio/components/page-header/page-header.component.ts
@@ -1,48 +1,17 @@
-import {
-  Component,
-  AfterViewInit,
-  OnDestroy,
-  HostListener,
-} from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'pw-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss'],
 })
-export class PageHeaderComponent implements AfterViewInit, OnDestroy {
-  transparent = true;
-  headerHeight: number;
-
-  private _scrollSub: Subscription;
-
-  constructor() {}
-
-  ngAfterViewInit() {
-    // this._scrollSub = this.ionContent.ionScroll.subscribe(
-    //   (event: CustomEvent) => {
-    //     const { currentY } = event.detail;
-    //     if (currentY > 20) {
-    //       this.transparent = false;
-    //       return;
-    //     }
-    //     this.transparent = true;
-    //   }
-    // );
-  }
+export class PageHeaderComponent {
+  isHeaderAtTop = true;
 
   @HostListener('window:scroll', ['$event'])
-  onScroll(event) {
-    // Determine if the header is at the top of the page
-    const headerElement = document.getElementById('header');
-    const headerRect = headerElement.getBoundingClientRect();
-    const isHeaderAtTop = headerRect.top === 0;
-
-    console.log(isHeaderAtTop);
-  }
-
-  ngOnDestroy() {
-    this._scrollSub && this._scrollSub.unsubscribe();
+  onScroll() {
+    const rootElement = document.querySelector('app-root');
+    const rootRect = rootElement.getBoundingClientRect();
+    this.isHeaderAtTop = rootRect.top === 0;
   }
 }
